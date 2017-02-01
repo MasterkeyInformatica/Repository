84d76dfbf690c5643601d9405c10e32ab6a99adc
Matheus Lopes Santos <fale_com_lopez@hotmail.com>
2017-01-31 22:29:57 -0200

    Correção do ServiceProvider da Aplicação

--- src/Providers/RepositoryServiceProvider.php
+++ src/Providers/RepositoryServiceProvider.php
@@ -13,79 +13,92 @@
 
     class RepositoryServiceProvider extends ServiceProvider
     {
+        /**
+         * @var bool
+         */
         protected $defer = true;
 
+        /**
+         * Realiza o boot após os registros
+         *
+         * @return  void
+         */
         public function boot()
         {
             $config_path = __DIR__  . '/../../config/repository.php';
 
-            $this->publishes(
-                [$config_path => config_path('repository.php')],
-                'repositories'
-            );
+            $this->publishes([$config_path => config_path('repository.php')], 'repositories');
         }
 
+        /**
+         * Registra os serviços providos pelo Package
+         */
         public function register()
         {
-            // Register bindings.
             $this->registerBindings();
 
-            // Register make repository command.
             $this->registerMakeRepositoryCommand();
 
-            // Register make criteria command.
             $this->registerMakeCriteriaCommand();
 
-            // Register commands
             $this->commands(['command.repository.make', 'command.criteria.make']);
 
-            // Config path.
             $config_path = __DIR__ . '/../../config/repository.php';
 
-            // Merge config.
-            $this->mergeConfigFrom(
-                $config_path,
-                'repositories'
-            );
+            $this->mergeConfigFrom($config_path, 'repositories');
         }
 
+        /**
+         * Realiza o registro dos Bindings
+         *
+         * @return  void
+         */
         protected function registerBindings()
         {
-            // FileSystem.
-            $this->app->instance('FileSystem', new Filesystem());
+            $this->app->instance(Filesystem::class, new Filesystem());
 
-            // Composer.
-            $this->app->bind('Composer', function ($app) {
+            $this->app->bind(Composer::class, function ($app) {
                 return new Composer($app['FileSystem']);
             });
 
-            // Repository creator.
-            $this->app->singleton('RepositoryCreator', function ($app) {
+            $this->app->singleton(RepositoryCreator::class, function ($app) {
                 return new RepositoryCreator($app['FileSystem']);
             });
 
-            // Criteria creator.
-            $this->app->singleton('CriteriaCreator', function ($app) {
+            $this->app->singleton(CriteriaCreator::class, function ($app) {
                 return new CriteriaCreator($app['FileSystem']);
             });
         }
 
+        /**
+         * Registra o comando de criação de repositórios
+         *
+         * @return  void
+         */
         protected function registerMakeRepositoryCommand()
         {
-            // Make repository command.
-            $this->app['command.repository.make'] = $this->app->share(function($app) {
+            $this->app['command.repository.make'] = $this->app->singleton(MakeRepositoryCommand::class, function($app) {
                 return new MakeRepositoryCommand($app['RepositoryCreator'], $app['Composer']);
             });
         }
 
+        /**
+         * Registra o comando de criação de criterias
+         *
+         * @return  void
+         */
         protected function registerMakeCriteriaCommand()
         {
-            // Make criteria command.
-            $this->app['command.criteria.make'] = $this->app->share(function($app) {
+            $this->app['command.criteria.make'] = $this->app->singleton(MakeCriteriaCommand::class, function($app) {
                 return new MakeCriteriaCommand($app['CriteriaCreator'], $app['Composer']);
             });
         }
 
+        /**
+         * Retorna os serviços providos
+         *
+         * @return  array
+         */
         public function provides()
         {
             return [
