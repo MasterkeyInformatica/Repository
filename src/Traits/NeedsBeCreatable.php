c1d4edb86d9d35dc4d7bf6abd9f12683c5e9db95
Matheus Lopes Santos <informatica.fadenor@unimontes.br>
2018-04-24 11:06:22 -0300

    Criação da trait NeedsBeCreatable para abrigar funções de create, update e delete de models

---
+++ src/Traits/NeedsBeCreatable.php
@@ -0,0 +1,181 @@
+<?php
+
+namespace Masterkey\Repository\Traits;
+
+use Illuminate\Support\Facades\DB;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\Event;
+use Masterkey\Repository\Events\EntityCreated;
+use Masterkey\Repository\Events\EntityDeleted;
+use Masterkey\Repository\Events\EntityUpdated;
+use RepositoryException;
+
+trait NeedsBeCreatable
+{
+    /**
+     * @param   array $data
+     * @return  Model
+     * @throws  RepositoryException
+     */
+    public function create(array $data)
+    {
+        $this->validateBeforeInsert($data);
+
+        $model = $this->model->create($data);
+
+        if ( $model ) {
+            Event::fire(new EntityCreated($this, $model));
+
+            return $model;
+        }
+
+        throw new RepositoryException('Nāo foi possível salvar os dados. Tente novamente');
+    }
+
+    /**
+     * @param   array $data
+     * @return  Model
+     * @throws  RepositoryException
+     */
+    public function firstOrCreate(array $data)
+    {
+        $this->validateBeforeInsert($data);
+
+        $model = $this->model->firstOrCreate($data);
+
+        if( $model ) {
+            Event::fire(new EntityCreated($this, $model));
+
+            return $model;
+        }
+
+        throw new RepositoryException('Não foi possível salvar os dados. Tente novamente');
+    }
+
+    /**
+     * @param   array $data
+     * @return  Model
+     */
+    public function firstOrNew(array $data)
+    {
+        $this->validateBeforeInsert($data);
+
+        return $this->model->firstOrNew($data);
+    }
+
+    /**
+     * @param   array  $data
+     * @return  Model
+     * @throws  RepositoryException
+     */
+    public function save(array $data)
+    {
+        $this->validateBeforeInsert($data);
+
+        foreach ( $data as $k => $v ) {
+            $this->model->$k = $v;
+        }
+
+        if ( $this->model->save() ) {
+            Event::fire(new EntityCreated($this, $this->model->getModel()));
+
+            return $this->model;
+        }
+
+        throw new RepositoryException('Nao foi possível salvar os dados, Tente Novamente');
+    }
+
+    /**
+     * @param   array  $data
+     * @return  bool
+     * @throws  RepositoryException
+     */
+    public function insert(array $data) : bool
+    {
+        DB::transaction(function () use ($data) {
+            if ( $this->model->insert($data) ) {
+                Event::fire(new EntityCreated($this, $this->model->getModel()));
+            }
+
+            throw new RepositoryException('Não foi possível salvar alguns registros. Tente novamente');
+        });
+
+        return true;
+    }
+
+    /**
+     * @param   int  $id
+     * @param   array  $data
+     * @return  Model
+     * @throws  RepositoryException
+     */
+    public function update(int $id, array $data)
+    {
+        $this->validateBeforeUpdate($data);
+
+        $model      = $this->find($id);
+        $original   = clone $model;
+
+        if ( $model->update($data) ) {
+            Event::fire(new EntityUpdated($this, $original));
+
+            return $model;
+        }
+
+        throw new RepositoryException('Não foi possível atualizar o registro. Tente novamente');
+    }
+
+    /**
+     * @param   array  $data
+     * @return  bool
+     */
+    public function massUpdate(array $data)
+    {
+        $this->applyCriteria();
+
+        $updated = $this->model->update($data);
+
+        if ( $updated ) {
+            Event::fire(new EntityUpdated($this, $this->model->getModel()));
+        }
+
+        return $updated;
+    }
+
+    /**
+     * @param   int  $id
+     * @return  bool
+     * @throws  RepositoryException
+     */
+    public function delete(int $id) : bool
+    {
+        $model      = $this->find($id);
+        $original   = clone $model;
+
+        if ( $model->delete() ) {
+            Event::fire(new EntityDeleted($this, $original));
+
+            return true;
+        }
+
+        throw new RepositoryException('Não foi possível apagar o registro. Tente Novamente');
+    }
+
+    /**
+     * @param   array  $records
+     * @return  bool
+     * @throws  RepositoryException
+     */
+    public function destroy(array $records) : bool
+    {
+        $this->applyCriteria();
+
+        if ( $this->model->destroy($records) ) {
+            Event::fire(new EntityDeleted($this, $this->model->getModel()));
+
+            return true;
+        }
+
+        throw new RepositoryException('Os registros não foram apagados. Tente novamente');
+    }
+}
---
+++ src/Traits/NeedsBeCriteriable.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace Masterkey\Repository\Traits;
+
+trait NeedsBeCriteriable
+{
+
+    public function resetScope()
+    {
+        $this->skipCriteria(false);
+
+        return $this;
+    }
+
+    /**
+     * @param   boolean  $status
+     * @return  $this
+     */
+    public function skipCriteria($status = true)
+    {
+        $this->skipCriteria = $status;
+
+        return $this;
+    }
+
+    /**
+     * @return   \Illuminate\Support\Collection
+     */
+    public function getCriteria()
+    {
+        return $this->criteria;
+    }
+
+    /**
+     * @param   Criteria  $criteria
+     * @return  Collection
+     */
+    public function getByCriteria(Criteria $criteria) : Collection
+    {
+        $this->pushCriteria($criteria);
+
+        return $this->all();
+    }
+
+    /**
+     * @param   Criteria  $criteria
+     * @return  $this
+     */
+    public function pushCriteria(Criteria $criteria)
+    {
+        if ( $this->preventCriteriaOverwriting ) {
+            // Find existing criteria
+            $key = $this->criteria->search(function ($item) use ($criteria) {
+                return ( is_object($item) && (get_class($item) == get_class($criteria)) );
+            });
+
+            // Remove old criteria
+            if ( is_int($key) ) {
+                $this->criteria->offsetUnset($key);
+            }
+        }
+
+        $this->criteria->push($criteria);
+
+        return $this;
+    }
+
+    /**
+     * @return   $this
+     */
+    public function applyCriteria()
+    {
+        if ( $this->skipCriteria === true ) {
+            return $this;
+        }
+
+        foreach ( $this->getCriteria() as $criteria ) {
+            if ( $criteria instanceof Criteria ) {
+                $this->model = $criteria->apply($this->model, $this);
+            }
+        }
+
+        return $this;
+    }
+}
