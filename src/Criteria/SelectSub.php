ba44149c63387aa3de7692536408768555d95a45
Matheus Lopes Santos <fale_com_lopez@hotmail.com>
2019-11-28 15:22:08 -0300

    Criação de Select Raw querys

--- src/Criteria/SelectSub.php
+++ src/Criteria/SelectSub.php
@@ -7,41 +7,41 @@ use Masterkey\Repository\AbstractCriteria;
 use Masterkey\Repository\Contracts\RepositoryInterface as Repository;
 
 /**
- * SelectRaw
+ * SelectSub
  *
  * @author Matheus Lopes Santos <fale_com_lopez@hotmail.com>
  * @version 1.0.0
  * @package Masterkey\Repository\Criteria
  */
-class SelectRaw extends AbstractCriteria
+class SelectSub extends AbstractCriteria
 {
     /**
      * @var string
      */
-    protected $expression;
+    protected $query;
 
     /**
-     * @var array
+     * @var string
      */
-    protected $bindings;
+    protected $as;
 
     /**
-     * @param string $expression
-     * @param array  $bindings
+     * @param string $query
+     * @param string $as
      */
-    public function __construct(string $expression, array $bindings = [])
+    public function __construct(string $query, string $as)
     {
-        $this->expression = $expression;
-        $this->bindings = $bindings;
+        $this->query = $query;
+        $this->as = $as;
     }
 
     /**
      * @param Builder    $model
      * @param Repository $repository
-     * @return Builder|mixed
+     * @return Builder
      */
     public function apply($model, Repository $repository)
     {
-        return $model->selectRaw($this->expression, $this->bindings);
+        return $model->selectSub($this->query, $this->as);
     }
 }
