b602964bf49ee8f1958dd1d4410b12dfd231cdb6
Matheus Lopes Santos <fale_com_lopez@hotmail.com>
2020-06-19 16:11:25 -0300

    Alteração de docblock

--- src/Contracts/CreatableInterface.php
+++ src/Contracts/CreatableInterface.php
@@ -13,56 +13,23 @@ namespace Masterkey\Repository\Contracts;
  */
 interface CreatableInterface
 {
-    /**
-     * @param   array  $data
-     */
     public function create(array $data);
 
-    /**
-     * @param   array  $data
-     */
     public function firstOrCreate(array $data);
 
-    /**
-     * @param   array  $data
-     */
     public function firstOrNew(array $data);
 
-    /**
-     * @param   array  $data
-     */
     public function save(array $data);
 
-    /**
-     * @param   array  $data
-     * @return  bool
-     */
     public function insert(array $data) : bool;
 
     public function update(array $data, int $id = null);
 
-    /**
-     * @param   array  $data
-     */
     public function massUpdate(array $data);
 
-    /**
-     * @param   int  $id
-     * @return  bool
-     */
     public function delete(int $id) : bool;
 
-    /**
-     * @param   array  $records
-     * @return  bool
-     */
     public function destroy(array $records) : bool;
 
-    /**
-     * @param   int  $id
-     * @param   string  $relation
-     * @param   string  $attributes
-     * @param   bool  $detach
-     */
     public function sync($id, $relation, $attributes, $detach = true);
 }
